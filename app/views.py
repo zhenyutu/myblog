63ddea156908ac1cace6bc7e4036b7baa780cb13
zhenyutu <1572629866@qq.com>
2017-02-20 15:08:09 +0800

    完成博客文章分页

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app
 from app import form
 from app import db,models
 from flask import render_template
+from flask import request
 
 @app.route('/')
 @app.route('/login')
@@ -14,8 +15,10 @@ def index():
     article = models.Article(markDownEdit = postForm.markDownEdit.data,title = postForm.title.data,category = postForm.category.data,tag = postForm.tag.data)
     db.session.add(article)
     db.session.commit()
-    articles = models.Article.query.order_by(models.Article.timestamp.desc()).all()
-    return render_template("index.html",articles = articles)
+    page = request.args.get('page', 1, type=int)
+    pagination = models.Article.query.order_by(models.Article.timestamp.desc()).paginate(page, per_page=app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
+    articles = pagination.items
+    return render_template("index.html",articles = articles,pagination = pagination)
 
 @app.route('/create')
 def create():
--- config.py
+++ config.py
@@ -4,5 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_ON_REARDOWN = True
 
+#　跨域CSRF校验
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+# 分页数目
+FLASKY_POSTS_PER_PAGE = 3
