83632a6696839739b7471e4f8c53c05f9a5d4ba8
zhenyutu <1572629866@qq.com>
2017-02-21 13:10:35 +0800

    完成了recentArticle功能

--- app/static/js/style.js
+++ app/static/js/style.js
@@ -24,6 +24,14 @@ $(function () {
         }
     });
 
+    $('#tokenfield').tokenfield({
+        autocomplete: {
+          source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
+          delay: 100
+        },
+        showAutocompleteOnFocus: true
+    })
+
   });
 
 });
--- app/views.py
+++ app/views.py
@@ -10,10 +10,12 @@ def login():
 
 @app.route('/index', methods=['GET','POST'])
 def index():
+    titles=[]
     page = request.args.get('page', 1, type=int)
     pagination = models.Article.query.order_by(models.Article.timestamp.desc()).paginate(page, per_page=app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
     articles = pagination.items
-    return render_template("index.html",articles = articles,pagination = pagination)
+    recentArticles = models.Article.query.order_by(models.Article.timestamp.desc())[0:7]
+    return render_template("index.html",articles = articles,pagination = pagination,recentArticles = recentArticles)
 
 @app.route('/create')
 def create():
