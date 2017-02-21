e4dad519a7a464cc0fadf216b8fa1850cb0d3807
zhenyutu <1572629866@qq.com>
2017-02-21 13:43:31 +0800

    完成了标签插件的引入

--- app/static/js/style.js
+++ app/static/js/style.js
@@ -23,15 +23,10 @@ $(function () {
           }
         }
     });
-
-    $('#tokenfield').tokenfield({
-        autocomplete: {
-          source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
-          delay: 100
-        },
-        showAutocompleteOnFocus: true
-    })
-
   });
+
+  $('#tag').tokenfield()
+
+
 
 });
