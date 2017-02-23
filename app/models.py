227b8ac9f35ae34735ef917426f21deec55e6a81
zhenyutu <1572629866@qq.com>
2017-02-23 19:41:13 +0800

    完成了文章的markdown显示

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+from markdown import markdown
+import bleach
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -14,8 +16,20 @@ class Article(db.Model):
     __tablename__ = 'articles'
     id = db.Column(db.Integer, primary_key = True)
     markDownEdit = db.Column(db.Text)
+    htmlEdit = db.Column(db.Text)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     title = db.Column(db.String(128))
     category = db.Column(db.String(32))
     tag = db.Column(db.String(32))
     describe = db.Column(db.Text)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.htmlEdit = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Article.markDownEdit, 'set', Article.on_changed_body)
